05581e4241639b6e104c7fcd1347b5ea6d5cbe02
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2021-10-07 22:53:26 +0200

    Update hatyan_rpmbuild.sh

--- scripts/hatyan_rpmbuild.sh
+++ scripts/hatyan_rpmbuild.sh
@@ -9,19 +9,20 @@
 #!/bin/bash
 set -e
 
-#for teamcity
-#HATYANROOTFOLDER=%system.teamcity.build.checkoutDir% #workingDir/[definedcheckoutdir]
-#RPMTOPDIR=%system.teamcity.build.workingDir%/rpmbuild #(cannot contain ~ character)
+versiontag=main #the versiontag is also internally stored in the specfile, this should be aligned with this one. Possible are main, branches, tags like v2.2.68
 
-#for h6
-HATYANROOTFOLDER=~/hatyan_github
-RPMTOPDIR=/u/veenstra/rpmbuild #(cannot contain ~ character) is default location on h6 so not per se necessary here
-
+#define and delete resulting directories first to start clean
+#RPMTOPDIR=%system.teamcity.build.workingDir%/rpmbuild #(cannot contain ~ character) # for teamcity
+RPMTOPDIR=/u/veenstra/rpmbuild # for h6: (cannot contain ~ character) is default location on h6 so not per se necessary here
 rm -rf ${RPMTOPDIR} #to be sure all RPM's are removed, so quering version number only results in one number
-rpmbuild -v -bb ${HATYANROOTFOLDER}/scripts/hatyan_python-latest.spec --define "_topdir ${RPMTOPDIR}" --define "HATYANROOTFOLDER ${HATYANROOTFOLDER}"
+
+# download spec from source and rpmbuild from spec
+rm -rf hatyan_github
+git clone -b ${versiontag} https://github.com/Deltares/hatyan.git hatyan_github 
+rpmbuild -v -bb hatyan_github/scripts/hatyan_python-latest_git.spec
 
 #copy specfile and RPM to build folder
 HATYANVERSION=$(rpm -q -p ${RPMTOPDIR}/RPMS/x86_64/*.rpm --queryformat '%{VERSION}')
-mkdir -p ${HATYANROOTFOLDER}/build
-cp ${HATYANROOTFOLDER}/scripts/hatyan_python-latest.spec ${HATYANROOTFOLDER}/build/hatyan_python-${HATYANVERSION}.spec
-cp ${RPMTOPDIR}/RPMS/x86_64/*.rpm ${HATYANROOTFOLDER}/build
+mkdir -p hatyan_github/build
+cp hatyan_github/scripts/hatyan_python-latest.spec hatyan_github/build/hatyan_python-${HATYANVERSION}.spec
+cp ${RPMTOPDIR}/RPMS/x86_64/*.rpm hatyan_github/build
