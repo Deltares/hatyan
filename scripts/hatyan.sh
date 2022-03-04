7c548d22330ac5579c42f6f5a763636eb588ab5c
veenstrajelmer <jelmer.veenstra@deltares.nl>
2022-03-04 23:48:13 +0100

    updated hatyan.sh to automatically set QT_QPA_PLATFORM_PLUGIN_PATH depending on python version, updated other scripts

--- scripts/hatyan.sh
+++ scripts/hatyan.sh
@@ -9,7 +9,7 @@ elif [ "$1" == "--version" ]; then echo hatyan-$(python -c "import hatyan; print
 elif [ "$1" == "--test" ]; then hatyan /opt/hatyan_python/tests/examples/predictie_2019_19Ycomp4Ydia_VLISSGN_interactive.py
 elif [ "$1" == "--testmodules" ]; then
 	echo "testing if all dependencies for hatyan are installed";
-	for LIBRARYNAME in rh-python36-python rh-python36-python-libs glibc coreutils expect stix-fonts fontconfig freetype libstdc++ jasper libXcursor libXrender xorg-x11-xauth mesa-libGL mesa-libEGL libXi; do 
+	for LIBRARYNAME in glibc coreutils expect stix-fonts fontconfig freetype libstdc++ jasper libXcursor libXrender xorg-x11-xauth mesa-libGL mesa-libEGL libXi; do 
 		echo "--------------------------"
 		echo "searching for '$LIBRARYNAME' on this system";
 		#export LIBRARYVERSION=$(ldconfig -p | grep $LIBRARYNAME);
--- scripts/hatyan_rpmbuild_nobinaries.sh
+++ scripts/hatyan_rpmbuild_nobinaries.sh
@@ -21,8 +21,8 @@ rm -f ${HATYANEXEC}
 rm -rf hatyan_github
 git clone -b ${versiontag} https://github.com/Deltares/hatyan.git hatyan_github
 #in case of python 3.7/3.8/3.9 instead of 3.6.12
-sed -i "s/python -m pip install pyqt5==5.7.1/python -m pip install pyqt5 #==5.7.1/g" hatyan_github/scripts/hatyan_python-latest_python3.spec #unfix old pyqt5 version
-sed -i "s/requirements_dev.txt/requirements.txt/g" hatyan_github/scripts/hatyan_python-latest_python3.spec #unfix old dependency versions, since they are not all available in python >=3.8
+sed -i "s/python -m pip install pyqt5==5.7.1/python -m pip install pyqt5>=5.7.1/g" hatyan_github/scripts/hatyan_python-latest_python3.spec #unfix old pyqt5 version
+sed -i "s# -r %{_topdir}/BUILD/hatyan_github/requirements_dev.txt##g" hatyan_github/scripts/hatyan_python-latest_python3.spec #unfix old dependency versions, since they are not all available in python >=3.8
 
 # setup conda env with specific python version to use in rpmbuild, rpmbuild from spec, deactivate conda env
 module load anaconda3
