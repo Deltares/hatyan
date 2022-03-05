22e89b8c3ea74f06e556c485ac07106e1d6a78a3
veenstrajelmer <jelmer.veenstra@deltares.nl>
2022-03-05 18:12:43 +0100

    updated setup.py and hatyan_rpmbuild_nobinaries.sh to work also with python 3.10

--- scripts/hatyan_rpmbuild.sh
+++ scripts/hatyan_rpmbuild.sh
@@ -22,8 +22,8 @@ sed -i "s/python -m pip install pyqt5==5.7.1/python -m pip install pyqt5>=5.7.1/
 sed -i "s# -r %{_topdir}/BUILD/hatyan_github/requirements_dev.txt##g" hatyan_github/scripts/hatyan_python-latest_python3.spec #unfix old dependency versions, since they are not all available in python >=3.8
 
 # setup conda env with specific python version to use in rpmbuild, rpmbuild from spec, deactivate conda env
-module load anaconda3
-conda create -n hatyan_setup_venv python=3.7 -y # 3.6.12 3.7 3.8 3.9
+module load anaconda3 # use ``conda init`` after this command and restart shell, this activates base enviroment and makes it possible to activate venv (report issue to dsc)
+conda create -n hatyan_setup_venv python=3.7 -y # 3.6.12 3.7 3.8 3.9 3.10
 conda activate hatyan_setup_venv
 rpmbuild -v -bb hatyan_github/scripts/hatyan_python-latest_python3.spec --define "VERSIONTAG ${versiontag}"
 conda deactivate
