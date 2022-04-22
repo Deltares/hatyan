f823fd6f4c0ee508a13b55b23cda65ac262bb1b0
veenstrajelmer <jelmer.veenstra@deltares.nl>
2022-04-22 20:13:16 +0200

    Bump version: 2.5.50 → 2.5.51

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='hatyan',
-    version='2.5.50',
+    version='2.5.51',
     author="Jelmer Veenstra",
     author_email='Jelmer.Veenstra@Deltares.nl',
     url='https://repos.deltares.nl/repos/lib_tide/trunk/src/hatyan_python',
