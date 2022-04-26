b71d880be4ca714c4622004ed5d6bbe28970df5f
veenstrajelmer <jelmer.veenstra@deltares.nl>
2022-04-26 09:59:44 +0200

    Bump version: 2.5.52 → 2.5.53

--- hatyan/__init__.py
+++ hatyan/__init__.py
@@ -5,7 +5,7 @@
 
 __author__ = """Jelmer Veenstra"""
 __email__ = 'jelmer.veenstra@deltares.nl'
-__version__ = '2.5.52'
+__version__ = '2.5.53'
 
 from hatyan.analysis_prediction import *
 from hatyan.astrog import *
