32c823c8e1d748942e5f58e5a6e5543b79094ae9
veenstrajelmer <veenstrajelmer@gmail.com>
2023-08-09 14:03:56 +0200

    added freq test for single diafile

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -8,10 +8,31 @@ Created on Wed Aug  9 12:14:02 2023
 import os
 import pytest
 import glob
+import pandas as pd
 import hatyan
 
 dir_tests = os.path.dirname(__file__) #F9 doesnt work, only F5 (F5 also only method to reload external definition scripts)
 dir_testdata = os.path.join(dir_tests,'data_unitsystemtests')
+
+
+@pytest.mark.unittest
+def test_ts_from_singlefile_equidistant_dia_hasfreq():
+    """
+    When reading asingle equidistant diafile,
+    there should be a freq attribute that is not None
+    """
+    file_ts = os.path.join(dir_testdata,'VLISSGN_obs1.txt')
+    
+    ts_pd = hatyan.readts_dia(filename=file_ts)
+    
+    # check ts length (all four files are added)
+    assert len(ts_pd) == 8760
+    
+    # assert on freq attribute
+    assert hasattr(ts_pd.index,'freq')
+    assert isinstance(ts_pd.index.freq,pd.offsets.Minute)
+    assert ts_pd.index.freq is not None
+    assert ts_pd.index.freq.nanos/1e9 == 3600
 
 
 @pytest.mark.unittest
@@ -25,13 +46,14 @@ def test_ts_from_multifile_equidistant_dia_hasfreq():
     
     ts_pd = hatyan.readts_dia(filename=file_ts)
     
+    # check ts length (all four files are added)
     assert len(ts_pd) == 35064
     
-    ts_freq_min = ts_pd.index.freq.nanos/1e9/60
-    
+    # assert on freq attribute
     assert hasattr(ts_pd.index,'freq')
+    assert isinstance(ts_pd.index.freq,pd.offsets.Minute)
     assert ts_pd.index.freq is not None
-    assert ts_freq_min==60
+    assert ts_pd.index.freq.nanos/1e9 == 3600
 
 
 @pytest.mark.unittest
@@ -45,4 +67,3 @@ def test_ts_from_multifile_equidistant_dia_correctglob():
     ts_pd = hatyan.readts_dia(filename=file_ts)
     
     assert len(ts_pd) == 35064
-    
