129f3219c621bbc9e0d3090df08925faec057b0f
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-11-08 13:32:12 +0100

    update running examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,35 +6,35 @@ Created on Sat Apr  3 15:42:40 2021
 
 """
 
-
 import pytest
 import os
 import glob
+import subprocess
+
+# ACCEPTANCE TESTS VIA EXAMPLE SCRIPTS, these are the ones who are only meant to generate output files
 
 dir_tests = os.path.dirname(__file__) #F9 doesnt work, only F5 (F5 also only method to reload external definition scripts)
+list_configfiles = glob.glob(os.path.join(dir_tests,'examples','*.py'))
 dir_output_general = os.path.join(dir_tests,'examples_output')
-if not os.path.exists(dir_output_general):
-    os.mkdir(dir_output_general)
-
-
-# High level acceptance tests, these are the ones who are only meant to generate output files
-# for the testers to verify (in Teamcity) whether the runs generate the expected files or not.
-""" Run hatyan_main.py with test-configfiles as input """
-list_examplescripts = glob.glob(os.path.join(dir_tests,'examples','*.py'))
-
+os.makedirs(dir_output_general, exist_ok=True)
 
 @pytest.mark.acceptance
-@pytest.mark.parametrize("file_config", [pytest.param(file_config, id=os.path.basename(file_config).replace('.py','')) for file_config in list_examplescripts])
-def test_examplescripts(file_config): #FROM DFM_TOOLS
-    """
-    file_config = os.path.join(dir_tests,'configfiles','predictie_2019_b02ex2_19Ycomp4Ydia_CUXHVN_test.py')
-    """
+@pytest.mark.parametrize("file_config", [pytest.param(file_config, id=os.path.basename(file_config).replace('.py','')) for file_config in list_configfiles])
+def test_run_examples(file_config):
+    # 1. Set up test data
+    dir_output = os.path.join(dir_output_general,os.path.basename(file_config).replace('.py',''))
+    if not os.path.exists(dir_output):
+        os.mkdir(dir_output)
+    os.chdir(dir_output)
     
-    os.chdir(dir_output_general)
-    test = os.system(f"python -m hatyan {file_config} --redirect-stdout")
+    p = subprocess.Popen(f"python {file_config}",
+                         stderr=subprocess.STDOUT, # Merge stdout and stderr
+                         stdout=subprocess.PIPE,
+                         shell=True)
+    # max 30 minutes per test, if it hangs longer the test is killed
+    p.wait(1800)
     
-    if test:
-        raise Exception('execution did not finish properly')
-
-
-
+    if p.returncode:
+        out, err = p.communicate()
+        out_str = "\n".join([x.decode("utf-8") for x in out.split(b'\r\n')])
+        raise OSError(f'execution did not finish properly:\n{out_str}')
