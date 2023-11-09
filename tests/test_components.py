364e94127a3f3226bc41c13a542523c678dbca18
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-11-09 10:02:02 +0100

    added testcases for plot_components

--- tests/test_components.py
+++ tests/test_components.py
@@ -15,6 +15,26 @@ dir_tests = os.path.dirname(__file__) #F9 doesnt work, only F5 (F5 also only met
 dir_testdata = os.path.join(dir_tests,'data_unitsystemtests')
 
 
+@pytest.mark.unittest
+def test_plot_components_validation():
+    
+    current_station = 'HOEKVHLD'
+    file_data_comp0 = os.path.join(dir_testdata,f'{current_station}_ana.txt')
+    
+    comp_merged = hatyan.read_components(filename=file_data_comp0)
+    fig, (ax1,ax2) = hatyan.plot_components(comp=comp_merged, comp_validation=comp_merged)
+
+
+@pytest.mark.unittest
+def test_plot_components_allyears():
+    current_station = 'VLISSGN'
+    file_data_comp0 = os.path.join(dir_testdata,f'{current_station}_obs?.txt')
+    ts_measurements = hatyan.readts_dia(filename=file_data_comp0, station=current_station)
+    
+    ts_comp, ts_comp_all = hatyan.analysis(ts=ts_measurements, const_list='month', analysis_perperiod="Y", return_allperiods=True)
+    fig, (ax1,ax2) = hatyan.plot_components(comp=ts_comp, comp_allperiods=ts_comp_all)
+
+
 @pytest.mark.systemtest
 def test_components_timeshift():
     
