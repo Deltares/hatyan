f666fd91f11953656063032aef11a0c6ea0904be
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-11-09 12:31:53 +0100

    added test

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -5,10 +5,14 @@ Created on Thu Nov  9 12:13:40 2023
 @author: veenstra
 """
 
+import os
 import pytest
 import numpy as np
 import hatyan
 from hatyan.convert import convert_tzone2tzinfo
+
+dir_tests = os.path.dirname(__file__) #F9 doesnt work, only F5 (F5 also only method to reload external definition scripts)
+dir_testdata = os.path.join(dir_tests,'data_unitsystemtests')
 
 
 @pytest.mark.unittest
@@ -32,3 +36,17 @@ def test_convert_coordinates():
     
     assert np.allclose(values_x_RD_int, values_x_RD_str, values_x_RD_exp)
     assert np.allclose(values_y_RD_int, values_y_RD_str, values_y_RD_exp)
+
+
+@pytest.mark.unittest
+def test_convert_coordinates_diafile():
+    file_pred = os.path.join(dir_testdata, "VLISSGN_pre.txt")
+
+    diablocks_pd_extra = hatyan.get_diablocks(filename=file_pred)
+    coordx_in = diablocks_pd_extra.loc[0,'x']
+    coordy_in = diablocks_pd_extra.loc[0,'y']
+    epsg_in = diablocks_pd_extra.loc[0,'epsg']
+    WGS84x, WGS84y = hatyan.convert_coordinates(coordx_in=coordx_in, coordy_in=coordy_in, epsg_in=epsg_in, epsg_out="W84")
+
+    assert np.isclose(WGS84x, 3.596056794834692)
+    assert np.isclose(WGS84y, 51.44231093287052)
