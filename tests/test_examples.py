e0fc4992697d58b440e17a6c125994394fb2e8d8
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-11-09 08:51:12 +0100

    increased max acceptance test duration to 10 min

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -31,8 +31,8 @@ def test_run_examples(file_config):
                          stderr=subprocess.STDOUT, # Merge stdout and stderr
                          stdout=subprocess.PIPE,
                          shell=True)
-    # max 5 minutes per test, if it hangs longer the test is killed
-    p.wait(300)
+    # max 10 minutes per test, if it hangs longer the test is killed
+    p.wait(600)
     
     if p.returncode:
         out, err = p.communicate()
