6fdef684d90084869f41d97217d78f1ff55c1602
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-09-11 16:54:34 +0200

    Bump version: 2.9.0 → 2.9.1

--- hatyan/__init__.py
+++ hatyan/__init__.py
@@ -5,7 +5,7 @@ Tidal analysis and prediction tool of Rijkswaterstaat
 
 __author__ = """Jelmer Veenstra"""
 __email__ = "jelmer.veenstra@deltares.nl"
-__version__ = "2.9.0"
+__version__ = "2.9.1"
 
 from hatyan.analysis_prediction import *
 from hatyan.astrog import *
